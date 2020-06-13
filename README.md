346399cd9950f5529cc6f122a3e423e60b8ea090
Muazam Kamal <muazamkamal.dev@gmail.com>
2020-06-13 11:44:07 +0800

    Lint fixes

--- README.md
+++ README.md
@@ -12,22 +12,11 @@ Nice.
 
 ```js
 module.exports = {
-    // List of channels to ignore
-    ignoredChannel: [
-        "215442075011907584"
-    ],
-    // List of triggers
-    trigger: [
-        "69",
-        "sixty nine",
-        "sixty-nine",
-        "sixty 9",
-        "六十九",
-    ],
-    // List of response (randomly chosen)
-    response: [
-        "Nice.",
-        "Naisu.",
-    ]
+  // List of channels to ignore
+  ignoredChannel: ["215442075011907584"],
+  // List of triggers
+  trigger: ["69", "sixty nine", "sixty-nine", "sixty 9", "六十九"],
+  // List of response (randomly chosen)
+  response: ["Nice.", "Naisu."],
 };
 ```
--- handlers/message.js
+++ handlers/message.js
@@ -1,18 +1,20 @@
 let { ignoredChannel, trigger, response } = require("../config");
 
-module.exports = async message => {
-    if (message.author.bot) return;
-    if (message.system) return;
-    if (ignoredChannel.includes(message.channel)) return;
+module.exports = async (message) => {
+  if (message.author.bot) return;
+  if (message.system) return;
+  if (ignoredChannel.includes(message.channel)) return;
 
-    const tagging = new RegExp("<(@(!|&|)|#|a{0,1}:\\w+:)\\d+>+", "g");
-    const triggerRegExp = new RegExp(trigger.join("|"), "i");
+  const tagging = new RegExp("<(@(!|&|)|#|a{0,1}:\\w+:)\\d+>+", "g");
+  const triggerRegExp = new RegExp(trigger.join("|"), "i");
 
-    let userMessage = message.content.replace(tagging, " ");
+  let userMessage = message.content.replace(tagging, " ");
 
-    if (userMessage.match(triggerRegExp)) {
-        message.channel.send(response[Math.floor(Math.random() * response.length)]);
+  if (userMessage.match(triggerRegExp)) {
+    message.channel.send(response[Math.floor(Math.random() * response.length)]);
 
-        console.log(`${message.guild.name}#${message.channel.name}|${message.author.tag}: ${message.content}`);
-    }
+    console.log(
+      `${message.guild.name}#${message.channel.name}|${message.author.tag}: ${message.content}`
+    );
+  }
 };
